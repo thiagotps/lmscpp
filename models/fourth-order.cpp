356d2c53a2b973168485891c7c277190fbc16229
Thiago T. P. Silva <thiagoteodoro501@gmail.com>
2021-11-28 23:35:21 -0300

    Added a version for the new article focused on fourth order statistics

---
+++ models/fourth-order.cpp
@@ -0,0 +1,428 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+#include <stdexcept>
+#include <symengine/expression.h>
+#include <symengine/functions.h>
+#include <symengine/symbol.h>
+#include <symengine/ntheory.h>
+#include <symengine/logic.h>
+#include <symengine/matrix.h>
+#include <symengine/printers/latex.h>
+
+#include <argparse.hpp>
+
+#include <lmscpp/stochastic.hpp>
+#include <lmscpp/utils.hpp>
+#include <lmscpp/operators.hpp>
+
+#include "common.hpp"
+#include "symengine/symengine_rcp.h"
+#include "symengine/symengine_rcp.h"
+
+using namespace argparse;
+using namespace SymEngine;
+using namespace SymEngine::OverloadedOperators;
+using namespace std;
+using namespace stochastic;
+
+using namespace chrono;
+
+
+// NOTE: This a redefininition. Maybe I should create a header file with definitions.
+using vec_func = vector<RCP<const FunctionSymbol>>;
+
+enum cachemode
+  {
+   IGNORE=0,
+   READ,
+   WRITE,
+  };
+//  NOTE: This should be shared with classical.cpp
+void read_write_compute(const string& cachename, cachemode cmode, Experiment& todo, const vec_func & seeds)
+{
+  MeasureDuration duration{};
+
+  if (cmode == cachemode::READ)
+    {
+      fstream fs{cachename, fstream::binary | fstream::in};
+      if (fs.is_open())
+        {
+          cout << "Using cached data..." << endl;
+          duration.reset();
+          todo.load(fs);
+          duration.show("todo.load()");
+        }
+      else
+        throw runtime_error{"It was not possible to open cache " + cachename + " for reading."};
+    }
+  else
+    {
+      duration.reset();
+      todo.compute(seeds);
+      duration.show("todo.compute()");
+
+      if (cmode == cachemode::WRITE)
+        {
+          cout << "Writing cache..." << endl;
+          fstream fs{cachename, fstream::binary | fstream::out};
+
+          duration.reset();
+          todo.save(fs);
+          duration.show("todo.save()");
+        }
+    }
+}
+
+enum indmode
+  {
+   IA = 0,
+   EEA,
+  };
+
+enum outmode
+  {
+   SK = 0,
+   MSE,
+   FOUR,
+  };
+
+enum distmode
+  {
+   UNSPECIFIED = 0,
+   GAUSS,
+   LAP,
+  };
+
+
+
+
+string latex(const DenseMatrix& d) {
+  string out{};
+
+  auto n{d.nrows()}, m{d.ncols()};
+  for (auto i = 0; i < n; i++) {
+    for (auto j = 0; j < m; j++){
+      out += latex(*d.get(i, j)) + " &";
+    }
+    out += " \\\\\n";
+  }
+
+  return out;
+}
+
+int main(int argc, char ** argv)
+{
+  argparse::ArgumentParser program(argv[0]);
+
+  program.add_argument("--readcache")
+    .help("The cache file to use.")
+    .default_value(""s)
+    .action([](const string &val){return val;});
+
+  program.add_argument("--writecache")
+    .help("The cache file to write.")
+    .default_value(""s)
+    .action([](const string &val){return val;});
+
+  program.add_argument("-N").help("filter length").required().action([](const string &val){return stoi(val);});
+
+  program.add_argument("-M").help("data length").required().action([](const string &val){return stoi(val);});
+
+  program.add_argument("--indmode").help("ia or eea").required()
+    .action([](const string &val)
+            {
+              static const map<string, indmode> choices {{"ia", indmode::IA}, {"eea", indmode::EEA}};
+              auto it = choices.find(val);
+              if (it == choices.end())
+                throw runtime_error{"Invalid value for --iamode"};
+
+              return it->second;
+            });
+
+  program.add_argument("--outmode").help("sk, mse or four").required()
+    .action([](const string &val)
+            {
+              static const map<string, outmode> choices {{"sk", outmode::SK}, {"mse", outmode::MSE}, {"four", outmode::FOUR}};
+              auto it = choices.find(val);
+              if (it == choices.end())
+                throw runtime_error{"Invalid value for --outmode"};
+
+              return it->second;
+            });
+
+  program.add_argument("-b","--beta").help("β").default_value(-1.0)
+    .action([](const string &val){return stod(val);});
+
+  program.add_argument("--sv2","--sigmav2").help("variance (σᵥ²)").default_value(-1.0)
+    .action([](const string &val){return stod(val);});
+
+  program.add_argument("-n","--niter").help("Number of iterations.").default_value(-1)
+    .action([](const string &val){return stoi(val);});
+
+  program.add_argument("-o","--output").help("The file where the output will be stored.").default_value(""s)
+    .action([](const string &val){return val;});
+
+  program.add_argument("-d", "--dist").help("gauss or lap")
+    .default_value(distmode::UNSPECIFIED)
+    .action([](const string &val)
+            {
+              static const map<string, distmode> choices {{"gauss", distmode::GAUSS}, {"lap", distmode::LAP}};
+              auto it = choices.find(val);
+              if (it == choices.end())
+                throw runtime_error{"Invalid value for --dist"};
+
+              return it->second;
+            });
+
+  program.add_argument("--steady-state")
+    .help("The steady-state value. Currently only support the skewness.")
+    .implicit_value(true)
+    .default_value(false);
+
+  program.add_argument("--latex")
+    .help("print the matrices to the standard output ")
+    .implicit_value(true)
+    .default_value(false);
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const runtime_error &err) {
+    cout << err.what() << endl;
+    cout << program;
+    exit(1);
+  }
+
+  const auto readcache{program.get<string>("--readcache")};
+  const auto writecache{program.get<string>("--writecache")};
+  const auto N{program.get<int>("-N")};
+  const auto M{program.get<int>("-M")};
+  const auto beta{program.get<double>("--beta")};
+  const auto sigmav2{program.get<double>("--sigmav2")};
+  const auto niter{program.get<int>("--niter")};
+  const auto ofilename {program.get<string>("--output")};
+  const auto ind_mode = program.get<indmode>("--indmode");
+  const auto out_mode = program.get<outmode>("--outmode");
+  const auto dist_mode = program.get<distmode>("--dist");
+  const auto steady_state = program.get<bool>("--steady-state");
+  const auto print_latex = program.get<bool>("--latex");
+
+
+  if (not ofilename.empty())
+    if (beta < 0 or sigmav2 < 0 or niter < 0 or dist_mode == distmode::UNSPECIFIED)
+      throw runtime_error{"Missing some of the following: --beta, --sv2, --niter, --dist "};
+
+
+
+  const string cachename{ (not readcache.empty()) ? readcache : writecache };
+  const auto cache_mode = [&]()
+                          {
+                            if (not readcache.empty())
+                              return cachemode::READ;
+                            if (not writecache.empty())
+                              return cachemode::WRITE;
+
+                            return cachemode::IGNORE;
+                          }();
+
+  const auto sigmav{ symbol("σ_v") }, step_size{ symbol("β") }, k{ symbol("k") };
+  map_basic_basic cache{ {sigmav, number(sqrt(sigmav2))}, {step_size, number(beta)} };
+
+  StochasticProcess::clear();
+
+  StochasticProcess u{"u",  [](const vec_basic&, const RCP<const Integer>& n) -> RCP<const Basic>
+                      {
+                       if (n->as_int() % 2 == 0)
+                         return symbol("γ_" + to_string(n->as_int()));
+
+                       return zero;
+                      }};
+
+  StochasticProcess v{"v", [&sigmav](const vec_basic&, const RCP<const Integer>& n) -> RCP<const Basic>
+                           {
+                             if (even(*n))
+                               return pow(sigmav, n);
+
+                             return zero;
+                           }};
+
+  vector<StochasticProcess> wtil;
+  for (auto i = 0; i < N; i++)
+    wtil.emplace_back("ẅ_" + to_string(i), [](const vec_basic& vec, const RCP<const Integer>& n) -> RCP<const Basic>
+                           {
+                             if (eq(*vec[0], *zero))
+                               return one;
+
+                            return null;
+                           });
+
+  ExpectedOperator E{[ind_mode](const FunctionSymbol& x, const FunctionSymbol& y){
+                             auto xy = [ind_mode](const FunctionSymbol &x, const FunctionSymbol &y)
+                                       {
+                                         auto xname{x.get_name()};
+                                         auto yname{y.get_name()};
+
+                                         if (xname == "u" and yname == "u")
+                                           return not eq(x,y);
+
+                                         // NOTE: This is extremely ugly. Maybe there is some better way to do this
+                                         // comparasion.
+                                         if (xname.find("ẅ") != string::npos and yname == "u")
+                                           {
+                                             if (ind_mode == indmode::EEA)
+                                               return not rcp_dynamic_cast<const Integer>(expand(x.get_args()[0] - y.get_args()[0]))->is_positive();
+                                             else if (ind_mode == indmode::IA)
+                                               return true;
+                                           }
+
+                                         if (xname == "v")
+                                           return true;
+
+                                         return false;
+                                       };
+                             return xy(x,y) or xy(y,x);
+                           }};
+
+  array bvalues{1., 0.8, 0.8, -0.7, 0.6, -0.5, 0.4, -0.3, 0.2, -0.1};
+  if (bvalues.size() < M)
+    {
+      cerr << "There size of 'bvalues' is less than " << M << endl;
+      return 1;
+    }
+
+  vector<RCP<const FunctionSymbol>> b;
+  for (auto i = 0; i < M; i++)
+    {
+      auto bi { function_symbol("b", integer(i)) };
+      b.push_back( rcp_static_cast<const FunctionSymbol>(bi) );
+      cache[bi] = number(bvalues[i]);
+    }
+
+  auto x  = [&](const auto & k)
+            {
+              RCP<const Basic> sum{zero};
+              // NOTE: This integer could be memorised.
+              for (auto i = 0; i < M; i++)
+                sum = sum + b[i] * u(k - integer(i));
+
+              return sum;
+            };
+
+  EquationSet eqs{k};
+
+  RCP<const Basic> inn{zero};
+  for (auto j = 0; j < N; j++)
+    inn = inn + x(k - integer(j))*wtil[j](k);
+
+  for (auto i = 0; i < N; i++)
+    eqs.setitem(wtil[i](k+one), wtil[i](k) - step_size*x(k - integer(i)) * inn - step_size*v(k)*x(k - integer(i)));
+
+  Experiment todo{eqs, E, cache, [dist_mode](const Symbol & x) -> RCP<const Basic>
+                                 {
+                                  auto name = x.__str__();
+                                  if (name.size() >= 4 and name.substr(0, 2) == "γ")
+                                    {
+                                      auto p{ stoll(name.substr(3)) };
+                                      if (dist_mode == distmode::LAP)
+                                        {
+                                          static const double scale{ 1.0/sqrt(2) };
+                                          auto res = numfactorial(p)*pow(scale, p);
+                                          return number(res);
+                                        }
+                                      else if (dist_mode == distmode::GAUSS)
+                                        {
+                                          static constexpr double sd{1.0};
+                                          auto res = pow(sd, p) * num_doublefactorial(p - 1);
+                                          return number(res);
+                                        }
+                                    }
+
+                                  return null;
+                                 }};
+
+  RCP<const Basic> mse_expanded{null};
+  vec_func seeds{};
+
+  if (out_mode == outmode::SK)
+    {
+      seeds.push_back(rcp_dynamic_cast<const FunctionSymbol>(E(wtil[0](k))));
+      seeds.push_back(rcp_dynamic_cast<const FunctionSymbol>(E(pow(wtil[0](k), 2_i))));
+      seeds.push_back(rcp_dynamic_cast<const FunctionSymbol>(E(pow(wtil[0](k), 3_i))));
+    }
+  else if (out_mode == outmode::MSE)
+    {
+      auto ee = rcp_dynamic_cast<const FunctionSymbol>( E(expand(pow(inn + v(k), 2_i ))) );
+      mse_expanded = E.expand(ee);
+      seeds = states_vars(mse_expanded);
+    }
+  else if (out_mode == outmode::FOUR)
+    {
+      seeds.push_back(rcp_dynamic_cast<const FunctionSymbol>(E(pow(wtil[0](k), 4_i))));
+    }
+
+  read_write_compute(cachename, cache_mode, todo, seeds);
+  cout << "NUMBER_OF_EQUATIONS: " << todo.get_number_of_eqs() << endl;
+
+  MeasureDuration duration{};
+  if (not ofilename.empty())
+    {
+      ofstream os{ofilename, ofstream::out | ofstream::trunc};
+      if (!os)
+        {
+          cerr << "It was not possible to open file " << ofilename << endl;
+          return 1;
+        }
+
+      if (out_mode == outmode::SK)
+        {
+          duration.reset();
+          todo.write_skewness(niter, wtil[0](k), os);
+          duration.show("todo.write_skewness()");
+        }
+      else if (out_mode == outmode::MSE)
+        {
+          duration.reset();
+          todo.write_expression(niter, mse_expanded, os);
+          duration.show("todo.write_expression()");
+        }
+      else if (out_mode == outmode::FOUR) {
+        duration.reset();
+        todo.write_expression(niter, rcp_dynamic_cast<const FunctionSymbol>(E(pow(wtil[0](k), 4_i))), os);
+        duration.show("todo.write_expression()");
+      }
+    }
+
+  if (print_latex)
+    {
+      cout << "Printing latex...\n" << endl;
+      cout << "Matrix A: " << endl;
+      cout << latex(todo.get_A()) << endl;
+
+      cout << "Matrix Yk: " << endl;
+      cout << latex(todo.get_Yk()) << endl;
+
+      cout << "Matrix B: " << endl;
+      cout << latex(todo.get_B()) << endl;
+
+      cout << "Printing numeric matrix...\n" << endl;
+
+      cout << "Matrix A: " << endl;
+      cout << todo.get_num_A() << endl;
+      cout << "Matrix B: " << endl;
+      cout << todo.get_num_B() << endl;
+    }
+
+  if (steady_state)
+    {
+      if (out_mode == outmode::SK) {
+        auto p = todo.skewness_steady_state(wtil[0](k));
+        cout << "steady-state: " << p.second << endl;
+        cerr << "Steady-state found with " << p.first << " iterations." << endl;
+      }
+      else
+        throw runtime_error{"The steady-state value of the non SK mode is currently not supported!"};
+    }
+
+  return 0;
+}
---
+++ samples/fourthemp.cpp
@@ -0,0 +1,438 @@
+#include <iostream>
+#include <fstream>
+#include <valarray>
+#include <random>
+#include <string>
+#include <future>
+#include <algorithm>
+#include <set>
+
+#include <argparse.hpp>
+#include <pcg_random.hpp>
+
+using namespace std;
+using namespace chrono;
+
+struct result_struct
+{
+  // m1, m2 and m3 are the raw moments.
+  valarray<double> m1, m2, m3, m4, error_square, pdf, fourth_moment;
+  inline void operator += (const result_struct & o )
+  {
+    m1 += o.m1; m2 += o.m2; m3 += o.m3; m4 += o.m4; error_square += o.error_square; pdf += o.pdf;
+  }
+
+  template<typename T>
+  inline void operator /= (T n)
+  {
+    m1 /= n; m2 /= n; m3 /= n; m4 /= n; error_square /= n; pdf /= n;
+  }
+};
+
+template <typename T>
+class neg_idx_vector
+{
+  const size_t offset_;
+  vector<T> vec_;
+public:
+  neg_idx_vector(size_t n, size_t offset): offset_{offset}, vec_(n) {}
+
+  inline T& at(int idx) { return vec_.at(idx + offset_); }
+  inline const T& at(int idx) const { return vec_.at(idx + offset_); }
+
+  inline auto begin() { return vec_.begin(); }
+  inline auto end() { return vec_.end(); }
+  inline const auto begin() const { return vec_.begin(); }
+  inline const auto end() const { return vec_.end(); }
+};
+
+template <typename A>
+A sgn(A a)
+{
+  if (a < 0)
+    return -1;
+
+  return 1;
+}
+
+class laplace_distribution
+{
+  uniform_real_distribution<> _uni{-1.0/2.0,1.0/2.0};
+  double _location, _scale;
+public:
+
+  laplace_distribution(double location, double scale): _location{location}, _scale{scale} {}
+
+  template<class Generator>
+  double operator()(Generator &g)
+  {
+    double u = _uni(g);
+    return _location - _scale*sgn(u)*log(1 - 2*abs(u));
+  }
+
+};
+
+enum
+  {
+   SKEWNESS = 1 << 0,
+   MSE = 1 << 2,
+   PDF = 1 << 3,
+   FOUR = 1 << 4,
+  };
+
+
+struct experiment
+{
+  const int N, M, niter;
+  const double beta, sigmav;
+  const uint16_t modes;
+  const string dist;
+
+  const int pdf_instant;
+  const double pdf_start, pdf_end;
+  const int pdf_samples, kernel_exp;
+  const bool pdf_square;
+
+  inline double normal_pdf(double x, double mean) const
+  {
+    const double sigma {pow(10, kernel_exp)};
+    return  exp(-(x - mean)*(x - mean)/(2.0*sigma*sigma))/(sigma * sqrt(2.0 * M_PI));
+  }
+
+  template<typename T>
+  auto compute(uintmax_t nsamples, T seed) const
+  {
+    static constexpr double sigmau {1};
+    static constexpr array b {1., 0.8, 0.8, -0.7, 0.6, -0.5, 0.4, -0.3, 0.2, -0.1};
+
+    pcg64 gen{seed};
+    normal_distribution<double> u_dist_gauss{0.0, sigmau};
+    laplace_distribution u_dist_lap(0.0, 1.0/sqrt(2.0));
+    normal_distribution<double> nu_dist_gauss{0.0, sigmav};
+
+    function<double()> u_dist;
+    if (dist == "gauss")
+      u_dist = [&gen,&u_dist_gauss]() {return u_dist_gauss(gen);};
+    else if (dist == "lap")
+        u_dist = [&gen,&u_dist_lap]() {return u_dist_lap(gen);};
+    else
+      throw runtime_error("Unknow dist: " + dist);
+
+    auto nu_dist = [&gen,&nu_dist_gauss]() {return nu_dist_gauss(gen);};
+
+    vector<double> v(niter + 1);
+    neg_idx_vector<double> u(niter + N + M - 1, N + M - 2 );
+
+    result_struct rs;
+    if (modes & (SKEWNESS | PDF))
+      {
+        rs.m1.resize(niter + 1, 0);
+        rs.m2.resize(niter + 1, 0);
+        rs.m3.resize(niter + 1, 0);
+      }
+
+    if (modes & MSE)
+      rs.error_square.resize(niter + 1, 0);
+
+    if (modes & PDF)
+      rs.pdf.resize(pdf_samples+1, 0);
+
+    if (modes & FOUR)
+      rs.m4.resize(niter + 1, 0);
+
+    vector<double> wtilk(N);
+    vector<double> wtilkplus1(N);
+
+    for (uintmax_t iter = 0; iter < nsamples; iter++)
+      {
+        // wtil(0) = [1 ... 1].transpose()
+        fill(wtilk.begin(), wtilk.end(), 1);
+
+        for (auto &val : v)
+          val = nu_dist();
+
+        for (auto &val : u)
+          val = u_dist();
+
+        neg_idx_vector<double> x(niter + N, N - 1);
+        for (auto k = 1 - N; k <= niter; k++)
+          {
+            x.at(k) = 0;
+            for (auto m = 0; m < M; m++)
+              x.at(k) += b.at(m) * u.at(k - m);
+          }
+
+        // Execute one trial of the experiment.
+        for (auto k = 0; k <= niter; k++)
+          {
+            // Update the raw moments
+            if (modes & (SKEWNESS | PDF))
+              {
+                rs.m1[k] += wtilk[0];
+                rs.m2[k] += wtilk[0]*wtilk[0];
+                rs.m3[k] += wtilk[0]*wtilk[0]*wtilk[0];
+              }
+
+            if (modes & FOUR)
+              rs.m4[k] += wtilk[0]*wtilk[0]*wtilk[0]*wtilk[0];
+
+            if (modes & MSE)
+              {
+                double error = v.at(k);
+                for (auto i = 0; i < N; i++)
+                  error += wtilk.at(i)*x.at(k - i);
+
+                rs.error_square[k] += error*error;
+              }
+
+            if (modes & PDF and k == pdf_instant)
+              {
+                double delta { (pdf_end - pdf_start)/pdf_samples };
+                for (auto n = 0; n <= pdf_samples; n++)
+                  {
+                    double x {pdf_start + n*delta};
+                    rs.pdf[n] += normal_pdf(x,  pdf_square ? wtilk.at(0)*wtilk.at(0) : wtilk.at(0) );
+                  }
+              }
+
+            double inner_prod{0};
+            for (auto j = 0; j < N; j++)
+              inner_prod += x.at(k - j) * wtilk.at(j);
+
+            for (auto i = 0; i < N; i++)
+              wtilkplus1.at(i) = wtilk.at(i) - beta*x.at(k - i)*inner_prod
+                - beta*x.at(k - i)*v.at(k);
+
+            wtilk = wtilkplus1;
+          }
+
+      }
+
+    return rs;
+  }
+};
+
+// Return a vector with n distinct random uintmax_t randomly choosen
+// from 1 to max value holded by this type.
+auto gen_random_seeds(int n)
+{
+  set<uintmax_t> s;
+  mt19937_64 gen{chrono::high_resolution_clock::now().time_since_epoch().count()};
+  uniform_int_distribution<uintmax_t> uri{1, numeric_limits<uintmax_t>::max()};
+
+  while (s.size() != n)
+    s.insert(uri(gen));
+
+  return vector(s.begin(), s.end());
+}
+
+
+int main(int argc, char ** argv) {
+
+  argparse::ArgumentParser program(argv[0]);
+  program.add_argument("--filter-length","-N")
+    .help("filter length ")
+    .required()
+    .action([](const string& val){return stoi(val);});
+
+  program.add_argument("--data-length", "-M")
+    .help("data model length ")
+    .required()
+    .action([](const string& val){return stoi(val);});
+
+  program.add_argument("--niter")
+    .help("number of iterations ")
+    .required()
+    .action([](const string& val){return stoi(val);});
+
+  program.add_argument("--exp")
+    .help("the number of Monte Carlos trials will be 10^exp ")
+    .required()
+    .action([](const string& val){return stoi(val);});
+
+  program.add_argument("--beta")
+    .help("step-size parameter β ")
+    .required()
+    .action([](const string& val){return stod(val);});
+
+  program.add_argument("--sigmav2","--sv2")
+    .help("variance (σᵥ²) ")
+    .required()
+    .action([](const string& val){return stod(val);});
+
+  program.add_argument("--dist")
+    .help("The distribution used for the input signal. Possible values are gauss and lap(for laplacian).")
+    .required()
+    .action([](const string& val)
+            {
+              if (val != "gauss" and val != "lap")
+                throw runtime_error("--dist must be gauss or lap");
+
+              return val;
+            });
+
+  program.add_argument("--ncpu")
+    .help("The number of cpus to be used. Default is the number of cpus in the system.")
+    .default_value(thread::hardware_concurrency())
+    .action([](const string& val){return stoi(val);});
+
+  program.add_argument("--skewness-file","--sk-file")
+    .help("the file where the evolution of the skewness of the first filter's coefficient will be stored")
+    .default_value(string(""))
+    .action([](const string& val){return val;});
+
+  program.add_argument("--mse-file")
+    .help("the file where the evolution of the MSE of the first filter's coefficient will be stored")
+    .default_value(string(""))
+    .action([](const string& val){return val;});
+
+  program.add_argument("--pdf-instant")
+    .help("The instant k to which the estimated PDF refers to.")
+    .default_value(0)
+    .action([](const string& val){return stoi(val);});
+
+  program.add_argument("--pdf-start")
+    .help("The first sample of the PDF to generate.")
+    .default_value(0.0)
+    .action([](const string& val){return stod(val);});
+
+  program.add_argument("--pdf-end")
+    .help("The last sample of the PDF to generate.")
+    .default_value(0.0)
+    .action([](const string& val){return stod(val);});
+
+  program.add_argument("--pdf-samples")
+    .help("The number of samples to describe the PDF in the interval [pdf-start, pdf-end]")
+    .default_value(0)
+    .action([](const string& val){return stoi(val);});
+
+  program.add_argument("--kernel-exp")
+    .help("The value of the standard-deviation of the gaussian kernel will be pow(10, kernel_exp).")
+    .default_value(-7)
+    .action([](const string& val){return stoi(val);});
+
+  program.add_argument("--pdf-square")
+    .help("Return the PDF of the square.")
+    .default_value(false)
+    .implicit_value(true);
+
+  program.add_argument("--pdf-file")
+    .help("The file where the PDF's evolution of the first filter coefficient at a determined instant k will be stored.")
+    .default_value(string(""))
+    .action([](const string& val){return val;});
+
+  program.add_argument("--fourth-file")
+    .help("The file where the fourth moment of the first desviation coefficient will be stored.")
+    .default_value(string(""))
+    .action([](const string& val){return val;});
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const runtime_error &err) {
+    cout << err.what() << endl;
+    cout << program;
+    exit(0);
+  }
+
+  const auto N = program.get<int>("-N");
+  const auto M = program.get<int>("-M");
+  const auto niter = program.get<int>("--niter");
+  const auto exp = program.get<int>("--exp");
+  const uintmax_t nsamples { static_cast<uintmax_t>(pow(10, exp)) };
+  const auto beta = program.get<double>("--beta");
+  const auto sigmav = sqrt(program.get<double>("--sigmav2"));
+  const auto skewness_file = program.get<string>("--skewness-file");
+  const auto mse_file = program.get<string>("--mse-file");
+  const auto pdf_file = program.get<string>("--pdf-file");
+  const auto fourth_file = program.get<string>("--fourth-file");
+  const auto dist = program.get<string>("--dist");
+  const auto NCPU = program.get<unsigned int>("--ncpu");
+  const auto pdf_instant = program.get<int>("--pdf-instant");
+  const auto pdf_start = program.get<double>("--pdf-start");
+  const auto pdf_end = program.get<double>("--pdf-end");
+  const auto pdf_samples = program.get<int>("--pdf-samples");
+  const auto kernel_exp = program.get<int>("--kernel-exp");
+  const auto pdf_square = program.get<bool>("--pdf-square");
+
+  uint16_t modes{0};
+  if (not skewness_file.empty())
+    modes |= SKEWNESS;
+
+  if (not mse_file.empty())
+    modes |= MSE;
+
+  if (not pdf_file.empty())
+    modes |= PDF;
+
+  if (not fourth_file.empty())
+    modes |= FOUR;
+
+  auto random_seeds = gen_random_seeds(NCPU);
+
+  const experiment e{.N = N, .M = M, .niter = niter, .beta = beta,
+    .sigmav = sigmav, .modes = modes, .dist = dist,
+    .pdf_instant = pdf_instant, .pdf_start = pdf_start, .pdf_end = pdf_end,
+    .pdf_samples = pdf_samples, .kernel_exp = kernel_exp,
+    .pdf_square = pdf_square};
+
+  auto q{nsamples/NCPU};
+  auto r{nsamples % NCPU};
+  auto func = [&e](auto n, auto seed) { return e.compute(n, seed); };
+
+
+  auto as1 = async(func, q + r, random_seeds[0]);
+  vector<decltype(as1)> v_async {};
+  v_async.emplace_back(move(as1));
+
+  for (auto i = 1; i <= NCPU - 1; i++)
+    v_async.emplace_back(async(func, q, random_seeds.at(i)));
+
+  auto res = v_async[0].get();
+  for (auto it = v_async.begin()+1; it != v_async.end(); it++)
+    res += it->get();
+
+  res /= nsamples;
+
+  if (modes & SKEWNESS)
+    {
+      ofstream skfile{skewness_file};
+      for (auto k = 0; k <= niter; k++)
+        {
+          double u {res.m1[k]};
+          double sd { sqrt(res.m2[k] - u*u) };
+          double sk { (res.m3[k] - 3*u*sd*sd - u*u*u)/(sd*sd*sd) };
+          skfile << k << " " << sk << endl;
+        }
+    }
+
+  if (modes & MSE)
+    {
+      ofstream msefile{mse_file};
+      for (auto k = 0; k <= niter; k++)
+        msefile << k << " " << res.error_square[k] << endl;
+    }
+
+  if (modes & PDF)
+    {
+      ofstream pdf {pdf_file};
+      double u { res.m1[pdf_instant] };
+      double sd { sqrt(res.m2[e.pdf_instant] - u*u) };
+
+      pdf << "#Mean,Standard-Deviation" << endl;
+      pdf << "#" << u << "," << sd << endl;
+
+      double delta {(pdf_end - pdf_start)/pdf_samples};
+
+      for (auto n = 0; n <= pdf_samples; n++)
+        pdf << pdf_start + delta*n << " " << res.pdf[n] << endl;
+    }
+
+  if (modes & FOUR) {
+    ofstream fourfile {fourth_file};
+
+    for (auto k = 0; k <= niter; k++)
+      fourfile << k << " " << res.m4[k] << endl;
+  }
+
+  return 0;
+}
